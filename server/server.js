e8e40fa9e9ce9ea1b69aa9057a7644839729ffe5
Nolen Lawton <mnlawton@icloud.com>
2023-01-28 13:11:10 -0600

    started base logic for calculation

--- server/server.js
+++ server/server.js
@@ -16,10 +16,33 @@ app.post('/guesses', (request, response) => {
 
     let dataObject = request.body;
     calculations.push(dataObject); 
+    console.log(dataObject)
+
+    let total = calculation(dataObject)
+    // console.log(total)
+
     response.sendStatus(201);
 })
 
 app.get('/guesses', (request, response) => {
 
+    // console.log(calculations)
     response.send(calculations);
 })
+
+function calculation(object) {
+    console.log(object.number1 + ' '+ object.operator + ' ' + object.number2)
+
+    if(object.operator === '+'){
+        console.log('add')
+    }
+    else if(object.operator === '-'){
+        console.log('subtract')
+    }
+    else if(object.operator === 'x'){
+        console.log('multiply')
+    }
+    else if(object.operator === '/'){
+        console.log('divide')
+    }
+}
